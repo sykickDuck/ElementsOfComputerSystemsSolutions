f8d58c745bf68fde6a9749c870583de7081ed123
sykickDuck <psy@sykickduck.uk>
2018-04-15 21:17:27 +0100

    Minor tidy up current of VMtranslator implementation
    Modified implementation so that multiple return paths removed.
    Tidied up doc string comment for VMtranslator class.

--- 07/VMtranslator.py
+++ 07/VMtranslator.py
@@ -2,20 +2,25 @@ import sys
 import os
 
 class VMtranslator:
-    '''Translates a single .vm file or directory which contains .vm files in to an .asm file for the Hack Platform'''    
+    '''Translates a single .vm file or directory containing .vm files in to an .asm file for the Hack Platform'''    
     
     def translate(self):
         """Function responsible for orchestrating the translation"""
+
+        statusCode = None
         
         if (len(sys.argv) == 1):        
             print("No file provided...")
-            return 2
+            statusCode = 2
 
         else:    
             path = sys.argv[1]
             
             if not os.path.exists(path):
-                return 2
+                print("Provided File/Folder does not exist")
+                statusCode = 2
+
+        return statusCode
 
 
 if __name__ == '__main__':
