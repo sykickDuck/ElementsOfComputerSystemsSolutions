6fd378973af9884c19b3a959e79652273c7b5e5e
sykickDuck <psy@sykickduck.uk>
2018-04-29 11:23:06 +0100

    Simply setup of file patch in parser tests Create method for file patch setup and removed the method decorator.

--- 07/ParserTests.py
+++ 07/ParserTests.py
@@ -5,46 +5,42 @@ from Parser import Parser
 class ParserTests(unittest.TestCase):
 
 
+    def setUpFilePatchWith(self, read_data):
+        patcher = patch('builtins.open', new_callable=mock_open, read_data= read_data)
+        self.addCleanup(patcher.stop)
+        self.mock_file = patcher.start()
+        self.translator = Parser("mockFile.vm")
 
-    @patch("builtins.open", new_callable=mock_open, read_data="")
-    def test_emptyFileHasNoCommands(self, mock):
+
+    def test_emptyFileHasNoCommands(self):
         '''OBJECTIVE: Test that Parser recognises an empty file has no more commands'''
-
-        translator = Parser("mockFile.vm")
-
-        self.assertEqual(False,translator.hasMoreCommands(), "Empty file should return false for has more commands")
+        self.setUpFilePatchWith("")
+        self.assertEqual(False, self.translator.hasMoreCommands(), "Empty file should return false for has more commands")
 
 
-    @patch("builtins.open", new_callable=mock_open, read_data="\n")
-    def test_fileWithABlankLineHasNoCommands(self, mock):
+    def test_fileWithABlankLineHasNoCommands(self):
         '''OBJECTIVE: Test that parser recognises that a blank line is not a command'''
+        self.setUpFilePatchWith("\n")
+        self.assertEqual(False, self.translator.hasMoreCommands(), "File with a blank line should return false for has more commands")
 
-        translator = Parser("mock")
-        self.assertEqual(False,translator.hasMoreCommands(), "File with a blank line should return false for has more commands")
 
-
-    @patch("builtins.open", new_callable=mock_open, read_data="//File on has a comment")
-    def test_fileWithOnlyCommentHasNoCommands(self, mock):
+    def test_fileWithOnlyCommentHasNoCommands(self):
         '''OBJECTIVE: Test that parser recognises a file with only a comment has no more commands'''
+        self.setUpFilePatchWith("//File on has a comment")
+        self.assertEqual(False, self.translator.hasMoreCommands(), "A file with only a comment should return false")
 
-        translator = Parser("mockFile.vm")
-        self.assertEqual(False, translator.hasMoreCommands(), "A file with only a comment should return false")
 
-
-    @patch("builtins.open", new_callable=mock_open, read_data="push 2")
-    def test_fileWithSingleCommandHasCommand(self, mock):
+    def test_fileWithSingleCommandHasCommand(self):
         '''OBJECTIVE: Test that parser recognises a file with a command has more commands'''
-
-        translator = Parser("mockFile.vm")
-        self.assertEqual(True, translator.hasMoreCommands(), "A file with a command should return true")
+        self.setUpFilePatchWith("push 2")
+        self.assertEqual(True, self.translator.hasMoreCommands(), "A file with a command should return true")
 
 
     @patch("builtins.open", new_callable=mock_open, read_data="//Push 2 on to the stack \n push 2")
     def test_fileWhereFirstCommandNotOnFirstLineHasCommand(self, mock):
         '''OBJECTIVE: Test that parser recognises a file where first command is not on the first line'''
-
-        translator = Parser("mockFile.vm")
-        self.assertEqual(True, translator.hasMoreCommands(), "A file with a command should return true")
+        self.setUpFilePatchWith("//Push 2 on to the stack \n push 2")
+        self.assertEqual(True, self.translator.hasMoreCommands(), "A file with a command should return true")
 
 
 if __name__ == "__main__":
